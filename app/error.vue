aa43f87d9b62015b07e3c60ba551f136f35c4b75
Colin <colinclissonhg@gmail.com>
2026-06-15 18:40:12 -0400

    feat: add 404 page

---
+++ app/error.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps<{
+  error: NuxtError;
+}>();
+
+useSeoMeta({
+  title: "404 - Page Not Found",
+  description: "404 - Page Not Found",
+  robots: "noindex",
+});
+</script>
+
+<template>
+  <NuxtLayout>
+    <section>
+      <h1 class="text-4xl font-bold md:text-5xl">404 - Page Not Found</h1>
+      <hr />
+      <NuxtLink
+        to="/"
+        class="focus-ring underline md:no-underline md:hover:underline"
+      >
+        <p>Go home</p>
+      </NuxtLink>
+    </section>
+  </NuxtLayout>
+</template>
